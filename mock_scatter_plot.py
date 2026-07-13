110dc243f08900a8ef3e87ff0b6e822479ade2c7
DISN-kolo <82868527+DISN-kolo@users.noreply.github.com>
2026-07-13 19:45:03 +0200

    additional overlay for testing correctness visually in pair plot

--- mock_scatter_plot.py
+++ mock_scatter_plot.py
@@ -1,18 +1,25 @@
-def draw_scatter_plot(plot, houses, final_grouping):
+def draw_scatter_plot(
+        plot,
+        houses,
+        final_grouping,
+        marker='o',
+        show_label=True):
     ctr = 0
     for entry in houses:
-        group = final_grouping.get_group(entry)
-        x = group.iloc[:, 0]
-        y = group.iloc[:, 1]
-        plot.scatter(
-            x,
-            y,
-            alpha=0.5,
-            color=(
-                (0.3+ctr*0.4) % 1.0,
-                (0.7+ctr*0.2) % 1.0,
-                (1.5-ctr*0.4) % 1.0
-            ),
-            label=entry
-        )
+        if (entry in final_grouping.groups):
+            group = final_grouping.get_group(entry)
+            x = group.iloc[:, 0]
+            y = group.iloc[:, 1]
+            plot.scatter(
+                x,
+                y,
+                alpha=0.2,
+                marker=marker,
+                color=(
+                    (0.3+ctr*0.4) % 1.0,
+                    (0.7+ctr*0.2) % 1.0,
+                    (1.5-ctr*0.4) % 1.0
+                ),
+                label=entry if show_label else None
+            )
         ctr += 1
